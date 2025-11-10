3dd241df5770a4f7108263f6b7ee552f4d51d8d4
Ritesh Bundel <bundelritesh9@gmail.com>
2025-11-10 12:07:06 +0530

    hangman_game.c

--- hangman_game.c
+++ hangman_game.c
@@ -14,53 +14,60 @@ int main()
     printf("Please write Capital words only!\n");
     printf("Word : _ _ _ _ _ _\n");
 
-    for (int i = 0; i < 6; i++){
+    for (int i = 0; i < 6; i++)
+    {
         underscore[i] = '_';
     }
 
-    while (lives > 0){
+    while (lives > 0)
+    {
         correctguess = 0;
         printf("Enter Word : ");
         scanf(" %c", &guess);
 
-        for (int i = 0; i < 6; i++){
+        for (int i = 0; i < 6; i++)
+        {
 
-            if (guess == word[i] && underscore[i] == '_'){
+            if (guess == word[i] && underscore[i] == '_')
+            {
                 underscore[i] = guess;
                 correctguess = 1;
-                
             }
-            else if (guess != word[i] && underscore[i] != '_'){
+            else if (guess != word[i] && underscore[i] != '_')
+            {
                 underscore[i] != guess;
                 correctguess = 0;
-                
             }
         }
 
         printf("Word: ");
-        for (int i = 0; i < 6; i++){
+        for (int i = 0; i < 6; i++)
+        {
             printf("%c ", underscore[i]);
         }
         printf("\n");
 
-        if(correctguess == 0){
-            lives--; 
+        if (correctguess == 0)
+        {
+            lives--;
             printf("Wrong guess! Lives left: %d\n", lives);
         }
 
         allCorrect = 1;
-        for (int i = 0; i < 6; i++){
-            if (underscore[i] != word[i]){
+        for (int i = 0; i < 6; i++)
+        {
+            if (underscore[i] != word[i])
+            {
                 allCorrect = 0;
                 break;
             }
         }
 
-        if(allCorrect){
+        if (allCorrect)
+        {
             printf("Congratulations! You Won!\n");
             return 0;
         }
-
     }
 
     printf("You lost!, The Word was %c%c%c%c%c%c", word[0], word[1], word[2], word[3], word[4], word[5]);
